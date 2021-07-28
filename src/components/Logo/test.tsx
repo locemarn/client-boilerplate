4effc24474da626af53ebcc0fc8e2662c9693c87
marcelo_nogueira <marcelon27@gmail.com>
2021-07-27 23:51:52 -0300

    Logo component

---
+++ src/components/Logo/test.tsx
@@ -0,0 +1,43 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Logo from '.'
+
+describe('<Logo />', () => {
+  it('should render a white label by default', () => {
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
+
+  it('should render a black label when color is passed', () => {
+    renderWithTheme(<Logo color="black" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#030517'
+    })
+  })
+
+  it('should render a normal when size is default', () => {
+    renderWithTheme(<Logo color="black" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '11rem'
+    })
+  })
+
+  it('should render a bigger low', () => {
+    renderWithTheme(<Logo color="black" size="large" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '20rem'
+    })
+  })
+
+  it('should render a bigger logo without text if hideMobile', () => {
+    renderWithTheme(<Logo hideOnMobile />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
+      'width',
+      '5.8rem',
+      { media: '(max-width: 768px)' }
+    )
+  })
+})
